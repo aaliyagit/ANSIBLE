bdb7fef81fa38bb58a8d801238bee28a5b84bd0a
aaliyagit <149709011+aaliyagit@users.noreply.github.com>
2023-12-25 16:37:21 -0500

    Update Main.tf

--- Jenkins-Ec2/Main.tf
+++ Jenkins-Ec2/Main.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "Jenkins-sg" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
+  ami                    = "008fe2fc65df48dac"
   instance_type          = "t2.medium"
-  key_name               = "Mumbai"
+  key_name               = "All-keys"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
 
